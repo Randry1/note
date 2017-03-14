8b6c87590248bb1d4d6a200a978cb0c6b88b8deb
Randry1 <revinandry@gmail.com>
2017-03-14 21:11:46 +0700

    Create rails_genegale.rb

---
+++ rails_genegale.rb
@@ -0,0 +1,22 @@
+#Создать рельсы 
+
+rails new nameBlog
+
+#Запустить рельсы 
+
+rails server
+  
+  #Сокращенная команда
+  rails s
+
+  #Запустить в разных средах
+  rails server -e test
+  rails server -e development
+  rails server -e prodaction
+  
+#Создать генерацию
+
+rails generate controller home index
+
+  #Коротко генерация
+  rails g controller home index
